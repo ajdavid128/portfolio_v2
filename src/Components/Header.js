0757e83aecfefe12c22e34ea8e7ee2b4427c2db1
Aaron David <ajdavid128@gmail.com>
2023-06-20 17:21:53 -0500

    added content to Header comp, added photo to meida folder, styled header text and divs

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,19 @@
+import { Button } from "semantic-ui-react";
+import aaron_circle from '../media/aaron_circle.png'
+
 function Header() {
     return ( 
-        <>
-            <h1>Header</h1>
-        </>
+        <div id='header-container'>
+            <div id='header-child-div-1'>
+                <div>
+                    <h1 className='header-h1'>Hi I'm Aaron David <br/>and I'm a Web <br/>Developer</h1>
+                    <Button>Contact</Button>
+                </div>
+            </div>
+            <div>
+                <img id='circle_photo' src={aaron_circle} alt='aaron david'/>
+            </div>
+        </div>
      );
 }
 
